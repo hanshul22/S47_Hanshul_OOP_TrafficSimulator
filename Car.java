7e26e973d14d193fc56ace4ec87cb02233a7e64e
Truelierr <hanshul.k@kalvium.community>
2024-12-17 17:40:50 +0530

    Use of New in Java

--- Car.java
+++ Car.java
@@ -2,19 +2,22 @@ public class Car {
 
     private String name;
     private int speed; 
-    private int fuel;  
+    private int fuel;
+    private String track; // Added track field
 
-    public Car(String name, int speed, int fuel) {
-        //Using this pointer
+    public Car(String name, int speed, int fuel, String track) {
+        // Using this pointer
         this.name = name;
         this.speed = speed;
         this.fuel = fuel;
+        this.track = track;
     }
 
     public void displayInfo() {
         System.out.println("Car Name: " + name);
         System.out.println("Speed: " + speed + " km/h");
         System.out.println("Fuel: " + fuel + " liters");
+        System.out.println("Track Type: " + track);
     }
 
     public boolean move() {
@@ -25,6 +28,36 @@ public class Car {
             System.out.println(name + " is out of fuel and cannot continue.");
             return false; 
         }
+    }
+
+    public int getAdjustedSpeed() {
+        // Adjust speed based on the track and car type
+        if (track.equalsIgnoreCase("offroad")) {
+            if (name.equalsIgnoreCase("SportsCar")) {
+                return speed - 20;  // SportsCar loses 20 speed on off-road
+            } else if (name.equalsIgnoreCase("Truck")) {
+                return speed;  // Truck speed remains the same on off-road
+            } else if (name.equalsIgnoreCase("Sedan")) {
+                return speed - 10;  // Sedan loses 10 speed on off-road
+            }
+        } else if (track.equalsIgnoreCase("city")) {
+            if (name.equalsIgnoreCase("SportsCar")) {
+                return speed - 10;  // SportsCar loses 10 speed in the city
+            } else if (name.equalsIgnoreCase("Truck")) {
+                return speed - 5;  // Truck loses 5 speed in the city
+            } else if (name.equalsIgnoreCase("Sedan")) {
+                return speed;  // Sedan maintains speed in the city
+            }
+        } else if (track.equalsIgnoreCase("smooth")) {
+            if (name.equalsIgnoreCase("SportsCar")) {
+                return speed;  // SportsCar maintains speed on smooth road
+            } else if (name.equalsIgnoreCase("Truck")) {
+                return speed - 5;  // Truck loses 5 speed on smooth road
+            } else if (name.equalsIgnoreCase("Sedan")) {
+                return speed - 5;  // Sedan loses 5 speed on smooth road
+            }
+        }
+        return speed;  // Default return if no track matched
     }
 
     public String getName() {
--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        
-        Car[] cars = new Car[2]; 
+        // Step 1: Dynamic Memory Allocation for Car Objects
+        Car car1 = new Car("SportsCar", 120, 10, "smooth");
+        Car car2 = new Car("Sedan", 100, 8, "city");
+        Car car3 = new Car("Truck", 80, 5, "offroad");
 
-        cars[0] = new Car("SportsCar", 120, 10);
-        cars[1] = new Car("Sedan", 100, 8);      
-        
-        Race race = new Race(cars[0], cars[1], 5); 
-        
+        // Step 2: Passing cars dynamically to the Race
+        Race race = new Race(car1, car2, car3, 10);
+
+        // Step 3: Start the race
         race.startRace();
+
+        // Step 4: Cleanup (not strictly required in Java, but for explanation)
+        car1 = null;
+        car2 = null;
+        car3 = null;
+        System.gc();
     }
 }
